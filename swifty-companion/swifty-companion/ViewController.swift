b4b17861e960ead8c4f35ea11c30a6a36079d7dd
Dmitry Novikov <dredfort.42@gmail.com>
2022-10-30 14:32:06 +0300

    begin

---
+++ swifty-companion/swifty-companion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swifty-companion
+//
+//  Created by Dmitry Novikov on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

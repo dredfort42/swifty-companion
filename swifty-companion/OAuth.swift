c457a8a777eb25d927bde04308d0417ad7410acd
Dmitry Novikov <dredfort.42@gmail.com>
2022-11-08 08:38:33 +0300

    getToken bug fixed

--- swifty-companion/OAuth.swift
+++ swifty-companion/OAuth.swift
@@ -15,51 +15,40 @@ import Alamofire
 import SwiftyJSON
 
 class OAuth {
-
-	static var counter: UInt8 = 0
 	
 	static func getToken() {
-		if counter == 5 {
-			exit(1)
-		}
-		if !UserDefaults.standard.bool(forKey: "token") {
-			let url: URLConvertible = "https://api.intra.42.fr/oauth/token"
-			let parameters: Parameters = [
-				"grant_type" : "client_credentials",
-				"client_id" : (ProcessInfo.processInfo.environment["client_id"] ?? "") as String,
-				"client_secret" : (ProcessInfo.processInfo.environment["client_secret"] ?? "") as String
-			]
-			var token: String = ""
-			AF.request(
-				url,
-				method: .post,
-				parameters: parameters
-			)
-			.validate(statusCode: 200..<201)
-			.validate(contentType: ["application/json"])
-			.responseData {
-				response in
-				switch response.result {
-					case .success:
-						print("[v] Token received")
-						if let responseValue = response.value {
-							let jsonData = JSON(responseValue)
-							token = jsonData["access_token"].stringValue
-							UserDefaults.standard.set(token, forKey: "token")
-						}
-						print("TOKEN: " + token)
-						OAuth.checkToken()
-					case .failure:
-						print("[x] Token receive error")
-						OAuth.getToken()
-						counter += 1
-				}
+		let url: URLConvertible = "https://api.intra.42.fr/oauth/token"
+		let parameters: Parameters = [
+			"grant_type" : "client_credentials",
+			"client_id" : (ProcessInfo.processInfo.environment["client_id"] ?? "") as String,
+			"client_secret" : (ProcessInfo.processInfo.environment["client_secret"] ?? "") as String
+		]
+		var token: String = ""
+		AF.request(
+			url,
+			method: .post,
+			parameters: parameters
+		)
+		.validate(statusCode: 200..<201)
+		.validate(contentType: ["application/json"])
+		.responseData {
+			response in
+			switch response.result {
+				case .success:
+					print("[v] Token received")
+					if let responseValue = response.value {
+						let jsonData = JSON(responseValue)
+						token = jsonData["access_token"].stringValue
+						UserDefaults.standard.set(token, forKey: "token")
+					}
+					print("TOKEN: " + token)
+				case .failure:
+					print("[x] Token receive error")
 			}
 		}
 	}
 
 	static func checkToken() {
-		counter = 0
 		let token: String = UserDefaults.standard.string(forKey: "token") ?? ""
 		let url: URLConvertible = "https://api.intra.42.fr/oauth/token/info"
 		let headers: HTTPHeaders = ["Authorization" : "Bearer " + token]
